4486af4b5bfe40688ce1a7d3d14271e14e178367
reddypandu <128275448+reddypandu@users.noreply.github.com>
2023-12-26 12:25:50 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,190 @@
+(function ($) {
+  "use strict";
+
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+    function toggleNavbarMethod() {
+      if ($(window).width() > 992) {
+        $(".navbar .dropdown")
+          .on("mouseover", function () {
+            $(".dropdown-toggle", this).trigger("click");
+          })
+          .on("mouseout", function () {
+            $(".dropdown-toggle", this).trigger("click").blur();
+          });
+      } else {
+        $(".navbar .dropdown").off("mouseover").off("mouseout");
+      }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+  // Date and time picker
+  $("#date").datetimepicker({
+    format: "L",
+  });
+  $("#time").datetimepicker({
+    format: "LT",
+  });
+
+  // Service carousel
+  $(".service-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    loop: true,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
+
+  // Pricing carousel
+  $(".pricing-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    loop: true,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+    },
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    dots: true,
+    loop: true,
+    items: 1,
+  });
+})(jQuery);
+
+//when scroll>100 then navabar fixed to top
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > 100) {
+    $(".navbar").addClass("active");
+  } else {
+    $(".navbar").removeClass("active");
+  }
+});
+
+/**
+ * add event on element
+ */
+
+// const addEventOnElem = function (e, type, callback) {
+//   if (e.length > 1) {
+//     for (let i = 0; i < e.length; i++) {
+//       e[i].addEventListener(type, callback);
+//     }
+//   } else {
+//     e.addEventListener(type, callback);
+//   }
+// };
+
+// const filterBtns = document.querySelectorAll("[data-filter-btn]");
+// const filterItems = document.querySelectorAll("[data-filter]");
+
+// let lastClickedFilterBtn = filterBtns[0];
+
+// const filter = function () {
+//   lastClickedFilterBtn.classList.remove("active");
+//   this.classList.add("active");
+//   lastClickedFilterBtn = this;
+
+//   for (let i = 0; i < filterItems.length; i++) {
+//     if (
+//       this.dataset.filterBtn === filterItems[i].dataset.filter ||
+//       this.dataset.filterBtn === "all"
+//     ) {
+//       filterItems[i].style.display = "block";
+//       filterItems[i].classList.add("active");
+//     } else {
+//       filterItems[i].style.display = "none";
+//       filterItems[i].classList.remove("active");
+//     }
+//   }
+// };
+
+// addEventOnElem(filterBtns, "click", filter);
+//remove .html extension when hover
+// document.addEventListener("DOMContentLoaded", function () {
+//   const navLinks = document.querySelectorAll(".navbar-nav a");
+
+//   navLinks.forEach((link) => {
+//     link.addEventListener("mouseover", function () {
+//       const originalHref = link.getAttribute("href");
+//       const cleanHref = originalHref.replace(".html", "");
+//       link.setAttribute("href", cleanHref);
+//     });
+
+//     link.addEventListener("mouseout", function () {
+//       const originalHref = link.getAttribute("href");
+//       const restoreHref = originalHref + ".html";
+//       link.setAttribute("href", restoreHref);
+//     });
+//   });
+// });
+// loader
+var loader = function () {
+  // Show loader
+  $("#ftco-loader").addClass("show");
+
+  // Hide loader after document is fully loaded
+  window.onload = function () {
+    setTimeout(function () {
+      if ($("#ftco-loader").length > 0) {
+        $("#ftco-loader").removeClass("show");
+      }
+    }, 1000);
+  };
+};
+
+loader();
